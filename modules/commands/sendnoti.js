afc0b95de13f049ce5705fddad745c5f6ea92e25
Nguyễn Như Thành <91858708+kocoten123@users.noreply.github.com>
2022-03-22 16:07:26 +0700

    Update sendnoti.js

--- modules/commands/sendnoti.js
+++ modules/commands/sendnoti.js
@@ -1,9 +1,9 @@
 module.exports.config = {
 	name: "sendnoti",
-	version: "1.0.1",
+	version: "1.0.2",
 	hasPermssion: 2,
-	credits: "Mirai Team",
-	description: "Gửi tin nhắn tới các nhóm!",
+	credits: "Mirai mod by HĐGN",
+	description: "Gửi tin nhắn tới các nhóm(reply vào ảnh/video cần gửi kèm)!\nPhiên bản xịn hơn của sendnotiUwU",
 	commandCategory: "system",
 	usages: "[Text]",
 	cooldowns: 5
@@ -21,13 +21,31 @@ module.exports.languages = {
 }
 
 module.exports.run = async ({ api, event, args, getText }) => {
+if (event.type == "message_reply") {
+const request = global.nodemodule["request"];
+const fs = require('fs')
+const axios = require('axios')
+
+
+
+       
+        var path = __dirname + `/cache/snoti.png`;
+        var path = __dirname + `/cache/snoti.mp4`;
+
+
+var abc = event.messageReply.attachments[0].url;
+    let getdata = (await axios.get(`${abc}`, { responseType: 'arraybuffer' })).data;
+
+  fs.writeFileSync(path, Buffer.from(getdata, 'utf-8'));
+
+
 	var allThread = global.data.allThreadID || [];
 	var count = 1,
 		cantSend = [];
 	for (const idThread of allThread) {
 		if (isNaN(parseInt(idThread)) || idThread == event.threadID) ""
 		else {
-			api.sendMessage("» NOTIFICATION !«\n\n" + args.join(" ") , idThread, (error, info) => {
+			api.sendMessage({body:"» 𝚃𝙽 𝚃ừ 𝙰𝚍𝚖𝚒𝚗 𝙱𝙾𝚃 «\n\n" + args.join(` `),attachment: fs.createReadStream(path) }, idThread, (error, info) => {
 				if (error) cantSend.push(idThread);
 			});
 			count++;
@@ -35,4 +53,21 @@ module.exports.run = async ({ api, event, args, getText }) => {
 		}
 	}
 	return api.sendMessage(getText("sendSuccess", count), event.threadID, () => (cantSend.length > 0 ) ? api.sendMessage(getText("sendFail", cantSend.length), event.threadID, event.messageID) : "", event.messageID);
+
 }
+else {
+	var allThread = global.data.allThreadID || [];
+	var count = 1,
+		cantSend = [];
+	for (const idThread of allThread) {
+		if (isNaN(parseInt(idThread)) || idThread == event.threadID) ""
+		else {
+			api.sendMessage("» 𝚃𝙽 𝚃ừ 𝙰𝚍𝚖𝚒𝚗 𝙱𝙾𝚃 «\n\n" + args.join(` `), idThread, (error, info) => {
+				if (error) cantSend.push(idThread);
+			});
+			count++;
+			await new Promise(resolve => setTimeout(resolve, 500));
+		}
+	}
+	return api.sendMessage(getText("sendSuccess", count), event.threadID, () => (cantSend.length > 0 ) ? api.sendMessage(getText("sendFail", cantSend.length), event.threadID, event.messageID) : "", event.messageID); }
+  }
