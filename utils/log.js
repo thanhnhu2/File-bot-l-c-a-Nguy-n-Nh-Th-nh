9e674193c937123740bedd2902a6b0814fd6d24b
Nguyễn Như Thành <91858708+kocoten123@users.noreply.github.com>
2022-03-22 08:03:29 +0700

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -3,13 +3,13 @@ const chalk = require('chalk');
 module.exports = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('[ ❕ Lỗi rồi ] » ') + data);
+			console.log(chalk.rgb(153,102,204)('» Lỗi rồi «  ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('[ ❕ Lỗi rồi ] » ') + data);
+			console.log(chalk.rgb(61,43,31)('» Lỗi rồi «  ') + data);
 			break;
 		default:
-			console.log(chalk.magenta(`${option} » `) + data);
+			console.log(chalk.rgb(0,255,255)(`» ${option} « `) + data);
 			break;
 	}
 }
@@ -17,13 +17,13 @@ module.exports = (data, option) => {
 module.exports.loader = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('[N-MANK ] » ') + data);
+			console.log(chalk.rgb(172,225,175)('» 𝒩𝒽𝓊̛ 𝒯𝒽𝒶̀𝓃𝒽 «  ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('[N-MANK ] » ') + data);
+			console.log(chalk.rgb(192,192,192)('» 𝒩𝒽𝓊̛ 𝒯𝒽𝒶̀𝓃𝒽 «  ') + data);
 			break;
 		default:
-			console.log(chalk.green(`[N-MANK ] » `) + data);
+			console.log(chalk.rgb(172,225,175)(`» 𝒩𝒽𝓊̛ 𝒯𝒽𝒶̀𝓃𝒽 «  `) + data);
 			break;
 	}
 }
